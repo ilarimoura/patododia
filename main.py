74c0405c963426789ff777a47b5464f2718e9534
larissamoura <larissarm91@gmail.com>
2023-09-20 00:17:47 -0300

    pato pronto

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import tweepy
 from datetime import datetime
+from moviepy.editor import VideoFileClip, AudioFileClip
+
+def gerar_video():
+    videoPato = VideoFileClip("video.mp4")
+    audioPato = AudioFileClip("audio.mp3")
+    audioPatoCurto = audioPato.set_duration(videoPato.duration)
+    final_clip = videoPato.set_audio(audioPatoCurto)
+    final_clip.write_videofile("pato_pronto.mp4",codec='libx264', audio_codec='aac')
+
 def diaDaSemana(dia):
     nomeDoDia =''
     if dia == 0:
@@ -18,11 +27,12 @@ def diaDaSemana(dia):
         nomeDoDia ='domingo'
     return 'Hoje é ' + nomeDoDia
 
+gerar_video()
 
 auth = tweepy.OAuth1UserHandler('kToF73rieDIO3tECcFk3jDdFh','snOnIhgU61UkleRlTGa85L84rPet4qcbB4IOJFDfXcpLXfAdOA', '1700698252700532736-2omHU8dS0orm3yi9wHfaI3T5By66FT', 'ecCouCcgmCJCniNY9uDiITDVriHax4qQEphvZzz63cH3L')
 
 api = tweepy.API(auth)
-patoDoDia = api.media_upload('pato_padeiro.mp4')
+patoDoDia = api.media_upload('pato_pronto.mp4')
 print(patoDoDia)
 
 client = tweepy.Client(
