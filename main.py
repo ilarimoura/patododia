a5d600d185ba6ffb0191255537160e5b77abbc58
larissamoura <larissarm91@gmail.com>
2023-09-21 23:37:51 -0300

    adicionar funçao baixar video

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import tweepy
 from datetime import datetime
 from moviepy.editor import VideoFileClip, AudioFileClip
+from pytube import YouTube
+
+def baixar_video(urlVideo):
+    YouTube(urlVideo) \
+        .streams \
+        .filter(mime_type='audio/mp4', abr='128kbps') \
+        .first() \
+        .download(filename='audio.mp4', skip_existing=False)
+
 
 def gerar_video():
     videoPato = VideoFileClip("video.mp4")
-    audioPato = AudioFileClip("audio.mp3")
+    audioPato = AudioFileClip("audio.mp4")
     audioPatoCurto = audioPato.set_duration(videoPato.duration)
     final_clip = videoPato.set_audio(audioPatoCurto)
     final_clip.write_videofile("pato_pronto.mp4",codec='libx264', audio_codec='aac')
@@ -27,6 +36,7 @@ def diaDaSemana(dia):
         nomeDoDia ='domingo'
     return 'Hoje é ' + nomeDoDia
 
+baixar_video('https://www.youtube.com/watch?v=Pu6Mid2a428')
 gerar_video()
 
 auth = tweepy.OAuth1UserHandler('kToF73rieDIO3tECcFk3jDdFh','snOnIhgU61UkleRlTGa85L84rPet4qcbB4IOJFDfXcpLXfAdOA', '1700698252700532736-2omHU8dS0orm3yi9wHfaI3T5By66FT', 'ecCouCcgmCJCniNY9uDiITDVriHax4qQEphvZzz63cH3L')
@@ -44,6 +54,7 @@ client = tweepy.Client(
 
 create1 = client.create_tweet(media_ids=[patoDoDia.media_id], text=diaDaSemana(datetime.today().weekday()))
 print(create1)
+
 
 
 
