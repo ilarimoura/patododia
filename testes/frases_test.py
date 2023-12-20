d0e21d717e2c1c76799155a21126ae74fd7a5893
larissamoura <larissarm91@gmail.com>
2023-12-19 23:40:03 -0300

    fizemos testes com o mock

--- testes/frases_test.py
+++ testes/frases_test.py
@@ -1,4 +1,5 @@
 from frases import Frases
+from unittest import mock
 
 def test_frase_do_pato_segunda_musica_artista_conhecido():
     assert Frases.fraseDoPato(0, 'Song 2','Blur') == 'Hoje é segunda! Dia de ouvir Blur - Song 2!'
@@ -26,3 +27,26 @@ def test_frases_do_pato_sabado_musica_artista_conhecido():
 
 def test_frases_do_pato_domingo_musica_artista_conhecido():
     assert Frases.fraseDoPato(6, 'depois', 'pato fu') == 'Hoje é domingo! Dia de ouvir pato fu - depois!'
+
+@mock.patch('random.randint')
+def test_frases_do_pato_segunda_musica_desconhecida_bora_dancar(randint_mock):
+    randint_mock.return_value = 1
+    assert Frases.fraseDoPato(0, None, 'pato fu') == 'Hoje é segunda! Bora dançar!'
+
+
+@mock.patch('random.randint')
+def test_frases_do_pato_quarta_musica_desconhecida_aumenta_som(randint_mock):
+    randint_mock.return_value = 2
+    assert Frases.fraseDoPato(2, None, 'blur') == 'Hoje é quarta! Aumenta o som!'
+
+
+@mock.patch('random.randint')
+def test_frases_do_pato_quinta_artista_desconhecida_aumenta_som(randint_mock):
+    randint_mock.return_value = 2
+    assert Frases.fraseDoPato(3, 'song 2', None) == 'Hoje é quinta! Aumenta o som!'
+
+
+@mock.patch('random.randint')
+def test_frases_do_pato_sexta_artista_desconhecida_bora_dancar(randint_mock):
+    randint_mock.return_value = 1
+    assert Frases.fraseDoPato(4, 'song 2', None) == 'Hoje é sexta! Bora dançar!'
