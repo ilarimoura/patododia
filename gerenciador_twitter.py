e5d55517952436257d191e3ad7530779611ecf5c
larissamoura <larissarm91@gmail.com>
2024-01-05 00:04:10 -0300

    estamos organizando mais ainda o pato

--- gerenciador_twitter.py
+++ gerenciador_twitter.py
@@ -2,22 +2,28 @@ import tweepy
 
 
 class GerenciadorTwitter:
-    @staticmethod
-    def postar(nome_arquivo: str, frase_do_dia: str, consumer_key, consumer_secret, access_token, access_token_secret):
-        auth = tweepy.OAuth1UserHandler(consumer_key,
-                                        consumer_secret,
-                                        access_token,
-                                        access_token_secret
+
+    def __init__(self, dados_twitter):
+        self.consumer_key = dados_twitter['consumer_key']
+        self.consumer_secret = dados_twitter['consumer_secret']
+        self.access_token = dados_twitter['access_token']
+        self.access_token_secret = dados_twitter['access_token_secret']
+
+    def postar(self, nome_arquivo: str, frase_do_dia: str):
+        auth = tweepy.OAuth1UserHandler(self.consumer_key,
+                                        self.consumer_secret,
+                                        self.access_token,
+                                        self.access_token_secret
                                         )
 
         api = tweepy.API(auth)
         pato_do_dia = api.media_upload(nome_arquivo)
 
         client = tweepy.Client(
-            consumer_key=consumer_key,
-            consumer_secret=consumer_secret,
-            access_token=access_token,
-            access_token_secret=access_token_secret
+            consumer_key=self.consumer_key,
+            consumer_secret=self.consumer_secret,
+            access_token=self.access_token,
+            access_token_secret=self.access_token_secret
         )
 
         create1 = client.create_tweet(media_ids=[pato_do_dia.media_id], text=frase_do_dia)
--- main.py
+++ main.py
@@ -6,29 +6,28 @@ from gerenciador_twitter import GerenciadorTwitter
 from gerenciador_data import GerenciadorDatas
 from musica import Musica
 from video import Video
+from arquivo import Arquivo
 
 
-with open('config.json') as arquivo:
-    dados1 = json.load(arquivo)
-
-with open('musicas_sorteadas.json') as arquivo_musicas_sorteadas:
-    dados_musicas_sorteadas = json.load(arquivo_musicas_sorteadas)
+config = Arquivo.abrir_json('config.json')
+dados_musicas_sorteadas = Arquivo.abrir_json('musicas_sorteadas.json')
+musicas_programadas = Arquivo.abrir_json('musicas_programadas.json', encoding="utf-8")
 
 hoje = date.today()
 dia_de_hoje_string = hoje.strftime('%Y-%m-%d')
-with open('musicas_programadas.json', encoding="utf-8") as musicas_programadas_arquivo:
-    musicas_programadas = json.load(musicas_programadas_arquivo)
-
 
 musica_programada_do_dia = GerenciadorDatas.achar_data(dia_de_hoje_string, musicas_programadas)
 
 
-pl = Playlist(dados1['youtube']['playlist_url'])
+pl = Playlist(config['youtube']['playlist_url'])
 
 if musica_programada_do_dia:
     videoSorteado = musica_programada_do_dia['url']
+    textoTwitter = musica_programada_do_dia['texto']
 else:
     videoSorteado = Musica.sorteia_nova_musica(pl,dados_musicas_sorteadas)
+    infoVideo = Video.dados_video(videoSorteado)
+    textoTwitter = Frases.fraseDoPato(datetime.today().weekday(), infoVideo['song'], infoVideo['artist'])
 
 posicao_musica = GerenciadorDatas.achar_posicao_do_array(hoje.day, hoje.month)
 dados_musicas_sorteadas[posicao_musica] = videoSorteado
@@ -37,20 +36,7 @@ with open('musicas_sorteadas.json', 'w') as arquivo_musicas_sorteadas_gravar:
     json.dump(dados_musicas_sorteadas, arquivo_musicas_sorteadas_gravar)
 
 Video.baixar_video(videoSorteado)
-
 Video.gerar_video()
-infoVideo = Video.dados_video(videoSorteado)
 
-if musica_programada_do_dia:
-    textoTwitter = musica_programada_do_dia['texto']
-else:
-    textoTwitter = Frases.fraseDoPato(datetime.today().weekday(), infoVideo['song'], infoVideo['artist'])
-
-
-GerenciadorTwitter.postar('pato_pronto.mp4',
-                          textoTwitter,
-                          dados1['twitter']['consumer_key'],
-                          dados1['twitter']['consumer_secret'],
-                          dados1['twitter']['access_token'],
-                          dados1['twitter']['access_token_secret']
-                        )
+twitter = GerenciadorTwitter(config['twitter'])
+twitter.postar('pato_pronto.mp4', textoTwitter)
