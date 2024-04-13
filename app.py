3c54ce2470fd81ec7ebd86e050258d94d86cb3fe
larissamoura <larissarm91@gmail.com>
2024-04-13 00:37:18 -0300

    Musica red hot

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import flask
-from flask import Flask
+import sqlite3
+from flask import Flask, g
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return flask.render_template('index.html')
+    cursor = get_db().cursor()
+    resposta = cursor.execute("select * from musicas_programadas")
+    return flask.render_template('index.html', musicas_programadas = resposta.fetchall())
 
 @app.post("/add_music")
 def add_music():
@@ -13,3 +16,15 @@ def add_music():
     print(flask.request.form.get('data'))
     return flask.redirect(flask.url_for('hello_world'))
 
+@app.teardown_appcontext
+def close_conection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+DATABASE = 'database.db'
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
